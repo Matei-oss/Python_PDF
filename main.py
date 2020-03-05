2ff82a537b455385e4debd4ff4a54f127d36cc23
Matei-oss <andrei_mateiu1996@live.com>
2020-03-05 16:48:40 +0000

    removed comments line

--- main.py
+++ main.py
@@ -1,43 +1,43 @@
 #PYTHON PDF's
 
 import PyPDF2 
-# import sys 
+import sys 
 
-# #rb - read binary
-# inputs = sys.argv[1:]
+# rb - read binary
+inputs = sys.argv[1:]
 
-# pdf_list = ['dummy.pdf','twopage.pdf','wtr.pdf']
+pdf_list = ['dummy.pdf','twopage.pdf','wtr.pdf']
 
-# def pdf_combiner(pdf_list):
-#   merger = PyPDF2.PdfFileMerger()
-#   for pdf in pdf_list:
-#     print(pdf)
-#     merger.append(pdf)
-#   merger.write('merged.pdf')
+def pdf_combiner(pdf_list):
+   merger = PyPDF2.PdfFileMerger()
+   for pdf in pdf_list:
+     print(pdf)
+     merger.append(pdf)
+   merger.write('merged.pdf')
   
 
-# pdf_combiner(pdf_list)
+ pdf_combiner(pdf_list)
 
-# with open('dummy.pdf','rb') as file:
-#   reader = PyPDF2.PdfFileReader(file)
-#   print(f' The file has {reader.numPages} page(s).')
+ with open('dummy.pdf','rb') as file:
+   reader = PyPDF2.PdfFileReader(file)
+   print(f' The file has {reader.numPages} page(s).')
 
-# with open('twopage.pdf','rb') as file2:
-#   reader = PyPDF2.PdfFileReader(file2)
-#   print(f' The file has {reader.numPages} page(s).')
+ with open('twopage.pdf','rb') as file2:
+   reader = PyPDF2.PdfFileReader(file2)
+   print(f' The file has {reader.numPages} page(s).')
 
-# with open('wtr.pdf','rb') as file3:
-#   reader = PyPDF2.PdfFileReader(file3)
-#   print(f' The file has {reader.numPages} page(s).')
+ with open('wtr.pdf','rb') as file3:
+   reader = PyPDF2.PdfFileReader(file3)
+   print(f' The file has {reader.numPages} page(s).')
 
-# with open('dummy.pdf','rb') as file:
-#   reader = PyPDF2.PdfFileReader(file)
-#   page = reader.getPage(0)
-#   page.rotateClockwise(90)
-#   writer = PyPDF2.PdfFileWriter()
-#   writer.addPage(page)
-#   with open('tilt-file.pdf','wb') as new_file:
-#     writer.write(new_file)
+ with open('dummy.pdf','rb') as file:
+   reader = PyPDF2.PdfFileReader(file)
+   page = reader.getPage(0)
+   page.rotateClockwise(90)
+   writer = PyPDF2.PdfFileWriter()
+   writer.addPage(page)
+   with open('tilt-file.pdf','wb') as new_file:
+     writer.write(new_file)
 
 pdf_file = "./twopage.pdf"
 watermark = "./wtr.pdf"
